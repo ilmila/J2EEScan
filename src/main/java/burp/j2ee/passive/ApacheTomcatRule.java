00f2286e0360f14e5a17dd2b299ff7d5cc6e6ae6
h3xstream <h3xstr@gmail.com>
2015-06-24 01:09:13 -0400

    Move Tomcat Manager JVM info to ApacheTomcatRule.java

--- src/main/java/burp/j2ee/passive/ApacheTomcatRule.java
+++ src/main/java/burp/j2ee/passive/ApacheTomcatRule.java
@@ -13,10 +13,10 @@ import burp.j2ee.Risk;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class ApacheTomcatRule implements PassiveRule {
 
     private static final Pattern TOMCAT_PATTERN = Pattern.compile("Apache Tomcat/([\\d\\.]+)", Pattern.DOTALL | Pattern.MULTILINE);
+    private static final Pattern JVM_RULE = Pattern.compile("\"><small>(1\\.\\d\\.[\\w\\-\\_\\.]+)<", Pattern.DOTALL | Pattern.MULTILINE);
 
     @Override
     public void scan(IBurpExtenderCallbacks callbacks, IHttpRequestResponse baseRequestResponse,
@@ -55,6 +55,44 @@ public class ApacheTomcatRule implements PassiveRule {
                 ));
             }
 
+        }
+
+        /**
+         *
+         * JVM Remote Release Detection
+         *
+         * Tomcat Manager JVM info
+         *
+         * <tr>
+         * <td class="row-center"><small>Apache Tomcat/6.0.26</small></td>
+         * <td class="row-center"><small>1.6.0_18-b18</small></td>
+         * <td class="row-center"><small>Sun Microsystems Inc.</small></td>
+         * <td class="row-center"><small>Linux</small></td>
+         * <td
+         * class="row-center"><small>2.6.30.10-105.2.23.fc11.i686.PAE</small></td>
+         * <td class="row-center"><small>i386</small></td>
+         */
+        if (respBody != null && reqInfo.getUrl().getPath().contains("manager/html")) {
+
+            Matcher matcher = JVM_RULE.matcher(respBody);
+
+            if (matcher.find()) {
+
+                callbacks.addScanIssue(new CustomScanIssue(
+                        baseRequestResponse.getHttpService(),
+                        reqInfo.getUrl(),
+                        baseRequestResponse,
+                        "Information Disclosure - JVM Remote Release Detection",
+                        "J2EEscan identified the remote JVM release <b>" + matcher.group(1) + "</b>",
+                        "Verify the Java updates for the release:<ul>"
+                                + "<li>Java 1.7 http://www.oracle.com/technetwork/java/javase/7u-relnotes-515228.html</li>"
+                                + "<li>Java 1.6 http://www.oracle.com/technetwork/java/javase/releasenotes-136954.html</li>"
+                                + "<li>Java 1.5 http://www.oracle.com/technetwork/articles/javase/overview-137139.html</li>"
+                                + "</ul>",
+                        Risk.Information,
+                        Confidence.Certain
+                ));
+            }
         }
     }
 }
--- src/main/java/burp/j2ee/passive/HttpServerHeaderRule.java
+++ src/main/java/burp/j2ee/passive/HttpServerHeaderRule.java
@@ -19,7 +19,6 @@ public class HttpServerHeaderRule implements PassiveRule {
 
     private static final Pattern JAVA_RULE = Pattern.compile("java\\/([\\d\\.\\_]+)", Pattern.DOTALL);
     private static final Pattern JETTY_RULE = Pattern.compile("Jetty.([\\d\\.]+)", Pattern.DOTALL);
-    private static final Pattern JVM_RULE = Pattern.compile("\"><small>(1\\.\\d\\.[\\w\\-\\_\\.]+)<", Pattern.DOTALL | Pattern.MULTILINE);
     private static final Pattern GLASSFISH_RULE = Pattern.compile("GlassFish Server Open Source Edition ([\\d\\.]+)", Pattern.DOTALL);
     private static final Pattern WEBLOGIC_RULE = Pattern.compile("WebLogic (:?Server )?([\\d\\.]+)", Pattern.DOTALL);
 
@@ -64,8 +63,6 @@ public class HttpServerHeaderRule implements PassiveRule {
             }
         }
 
-
-
         /**
          * Detect Jetty
          *
@@ -102,43 +99,7 @@ public class HttpServerHeaderRule implements PassiveRule {
 
 
 
-        /**
-         *
-         * JVM Remote Release Detection
-         *
-         * Tomcat Manager JVM info
-         *
-         * <tr>
-         * <td class="row-center"><small>Apache Tomcat/6.0.26</small></td>
-         * <td class="row-center"><small>1.6.0_18-b18</small></td>
-         * <td class="row-center"><small>Sun Microsystems Inc.</small></td>
-         * <td class="row-center"><small>Linux</small></td>
-         * <td
-         * class="row-center"><small>2.6.30.10-105.2.23.fc11.i686.PAE</small></td>
-         * <td class="row-center"><small>i386</small></td>
-         */
-        if (respBody != null && reqInfo.getUrl().getPath().contains("manager/html")) {
 
-            Matcher matcher = JVM_RULE.matcher(respBody);
-
-            if (matcher.find()) {
-
-                callbacks.addScanIssue(new CustomScanIssue(
-                        baseRequestResponse.getHttpService(),
-                        reqInfo.getUrl(),
-                        baseRequestResponse,
-                        "Information Disclosure - JVM Remote Release Detection",
-                        "J2EEscan identified the remote JVM release <b>" + matcher.group(1) + "</b>",
-                        "Verify the Java updates for the release:<ul>"
-                                + "<li>Java 1.7 http://www.oracle.com/technetwork/java/javase/7u-relnotes-515228.html</li>"
-                                + "<li>Java 1.6 http://www.oracle.com/technetwork/java/javase/releasenotes-136954.html</li>"
-                                + "<li>Java 1.5 http://www.oracle.com/technetwork/articles/javase/overview-137139.html</li>"
-                                + "</ul>",
-                        Risk.Information,
-                        Confidence.Certain
-                ));
-            }
-        }
 
         /**
          * Detect Glassfish
