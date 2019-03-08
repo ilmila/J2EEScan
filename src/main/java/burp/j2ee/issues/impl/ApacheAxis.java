fcef55bb4b92116282064394613194d9fb34fc4f
Enrico Milanese <enrico.milanese@emaze.net>
2019-03-08 15:17:22 +0100

    Improved Apache Axis Console detection

--- src/main/java/burp/j2ee/issues/impl/ApacheAxis.java
+++ src/main/java/burp/j2ee/issues/impl/ApacheAxis.java
@@ -69,7 +69,8 @@ public class ApacheAxis implements IModule {
             "/jboss-net/", // JBoss
             "/tomcat/axis/",
             "/wssgs/", //<h1>JBuilder Apache Axis Admin Console</h1> ..<title>Apache-Axis</title>
-            "/tresearch/" // JBuilder Apache Axis Admin Console
+            "/tresearch/", // JBuilder Apache Axis Admin Console
+            "/"
     );
 
     private static final List<String> HAPPY_AXIS_PATHS = Arrays.asList(
@@ -86,10 +87,18 @@ public class ApacheAxis implements IModule {
             "/tresearch/happyaxis.jsp"
     );
 
-    private static final String AXIS_SERVICES_PATH = "/services/listServices";
+    private static final List<String> AXIS_SERVICES_PATHS = Arrays.asList(
+            "/services/listServices",
+            "/services/"
+    );
+
     private static final String AXIS_ADMIN_PATH = "/axis2-admin/";
 
-    private static final byte[] GREP_STRING_AXIS_SERVICE_PAGE = "<title>List Services</title>".getBytes();
+    private static final List<byte[]> GREP_STRINGS_AXIS_SERVICE_PAGE = Arrays.asList(
+            "<title>Axis2: Services</title>".getBytes(),
+            "<title>List Services</title>".getBytes()
+    );
+
     // To match both Axis2 and Axis
     private static final byte[] GREP_STRING_HAPPY_AXIS = "Happiness Page".getBytes();
     private static final byte[] GREP_STRING_AXIS_XML = "<axisconfig".getBytes();
@@ -221,7 +230,6 @@ public class ApacheAxis implements IModule {
                 }
             }
 
-            
             List<String> AXIS_PATHS_MUTATED = URIMutator(AXIS_PATHS);
             for (String AXIS_PATH : AXIS_PATHS_MUTATED) {
 
@@ -281,92 +289,96 @@ public class ApacheAxis implements IModule {
 
                     }
                     // End axis2 administration console
-                    
-                    
 
                     // Enumerate the remote web services
-                    URL urlToTest = new URL(protocol, url.getHost(), url.getPort(), AXIS_PATH + AXIS_SERVICES_PATH);
-                    byte[] axistest = helpers.buildHttpRequest(urlToTest);
+                    for (String AXIS_SERVICES_PATH : AXIS_SERVICES_PATHS) {
+                        URL urlToTest = new URL(protocol, url.getHost(), url.getPort(), AXIS_PATH + AXIS_SERVICES_PATH);
+                        byte[] axistest = helpers.buildHttpRequest(urlToTest);
 
-                    byte[] response = callbacks.makeHttpRequest(url.getHost(),
-                            url.getPort(), isSSL, axistest);
+                        byte[] response = callbacks.makeHttpRequest(url.getHost(),
+                                url.getPort(), isSSL, axistest);
 
-                    IResponseInfo axisInfo = helpers.analyzeResponse(response);
+                        IResponseInfo axisInfo = helpers.analyzeResponse(response);
 
-                    if (axisInfo.getStatusCode() == 200) {
+                        if (axisInfo.getStatusCode() == 200) {
 
-                        String resp = helpers.bytesToString(response);
-                        String respBody = resp.substring(axisInfo.getBodyOffset());
+                            String resp = helpers.bytesToString(response);
+                            String respBody = resp.substring(axisInfo.getBodyOffset());
 
-                        // look for matches of our active check grep string
-                        List<int[]> matcheAxis = getMatches(helpers.stringToBytes(respBody),
-                                GREP_STRING_AXIS_SERVICE_PAGE, helpers);
+                            // look for matches of our active check grep string
+                            for (byte[] GREP_STRING_AXIS_SERVICE_PAGE : GREP_STRINGS_AXIS_SERVICE_PAGE) {
 
-                        if ((matcheAxis.size() > 0)) {
+                                List<int[]> matcheAxis = getMatches(helpers.stringToBytes(respBody),
+                                        GREP_STRING_AXIS_SERVICE_PAGE, helpers);
 
-                            // Retrieve the list of web services
-                            List<String> wsNames = HTTPMatcher.getServicesFromAxis(respBody);
+                                if ((matcheAxis.size() > 0)) {
 
-                            String wsListDescription = "";
+                                    // Retrieve the list of web services
+                                    List<String> wsNames = HTTPMatcher.getServicesFromAxis(respBody);
 
-                            if (wsNames.size() > 0) {
-                                wsListDescription = "<br />The registered Web Services are:<br/><ul>";
-                                for (String wsName : wsNames) {
-                                    wsListDescription += "<li><b>" + wsName + "</b></li>";
-                                }
-                                wsListDescription += "</ul><br />";
-                            }
+                                    String wsListDescription = "";
 
-                            issues.add(new CustomScanIssue(
-                                    baseRequestResponse.getHttpService(),
-                                    new URL(protocol, url.getHost(), url.getPort(), AXIS_PATH),
-                                    new CustomHttpRequestResponse(axistest, response, baseRequestResponse.getHttpService()),
-                                    TITLE_AXIS_SERVICES,
-                                    DESCRIPTION_AXIS_SERVICES + wsListDescription,
-                                    "Restrict access to the web service list resource",
-                                    Risk.Low,
-                                    Confidence.Certain
-                            ));
+                                    if (wsNames.size() > 0) {
+                                        wsListDescription = "<br />The registered Web Services are:<br/><ul>";
+                                        for (String wsName : wsNames) {
+                                            wsListDescription += "<li><b>" + wsName + "</b></li>";
+                                        }
+                                        wsListDescription += "</ul><br />";
+                                    }
 
-                            // Test for Directory Traversal issue
-                            if (wsNames.isEmpty()) {
-                                stdout.println("No Registered Web Services, skipping LFI vulnerability test");
-                                break;
-                            }
-
-                            String axisURIPATHLFI = AXIS_PATH + "/services/" + wsNames.get(0) + LFI_PAYLOAD;
-                            URL axisURLLFI = new URL(protocol, url.getHost(), url.getPort(), axisURIPATHLFI);
-
-                            byte[] axisLFITest = helpers.buildHttpRequest(axisURLLFI);
-
-                            byte[] LFIResponse = callbacks.makeHttpRequest(url.getHost(),
-                                    url.getPort(), isSSL, axisLFITest);
-
-                            IResponseInfo axisLFIInfo = helpers.analyzeResponse(LFIResponse);
-
-                            if (axisLFIInfo.getStatusCode() == 200) {
-
-                                String lfiResp = helpers.bytesToString(LFIResponse);
-                                String lfiRespBody = lfiResp.substring(axisLFIInfo.getBodyOffset());
-
-                                // look for matches of our active check grep string
-                                List<int[]> matchLFIAxis = getMatches(helpers.stringToBytes(lfiRespBody),
-                                        GREP_STRING_AXIS_XML, helpers);
-
-                                if ((matchLFIAxis.size() > 0)) {
                                     issues.add(new CustomScanIssue(
                                             baseRequestResponse.getHttpService(),
-                                            axisURLLFI,
-                                            new CustomHttpRequestResponse(axisLFITest, LFIResponse, baseRequestResponse.getHttpService()),
-                                            TITLE_AXIS_LFI,
-                                            DESCRIPTION_AXIS_LFI,
-                                            "Update the Apache Axis component with the last stable release",
-                                            Risk.High,
+                                            new URL(protocol, url.getHost(), url.getPort(), AXIS_PATH),
+                                            new CustomHttpRequestResponse(axistest, response, baseRequestResponse.getHttpService()),
+                                            TITLE_AXIS_SERVICES,
+                                            DESCRIPTION_AXIS_SERVICES + wsListDescription,
+                                            "Restrict access to the web service list resource",
+                                            Risk.Low,
                                             Confidence.Certain
                                     ));
+
+                                    // Test for Directory Traversal issue
+                                    if (wsNames.isEmpty()) {
+                                        stdout.println("No Registered Web Services, skipping LFI vulnerability test");
+                                        break;
+                                    }
+
+                                    String axisURIPATHLFI = AXIS_PATH + AXIS_SERVICES_PATH + wsNames.get(0) + LFI_PAYLOAD;
+                                    URL axisURLLFI = new URL(protocol, url.getHost(), url.getPort(), axisURIPATHLFI);
+
+                                    byte[] axisLFITest = helpers.buildHttpRequest(axisURLLFI);
+
+                                    byte[] LFIResponse = callbacks.makeHttpRequest(url.getHost(),
+                                            url.getPort(), isSSL, axisLFITest);
+
+                                    IResponseInfo axisLFIInfo = helpers.analyzeResponse(LFIResponse);
+
+                                    if (axisLFIInfo.getStatusCode() == 200) {
+
+                                        String lfiResp = helpers.bytesToString(LFIResponse);
+                                        String lfiRespBody = lfiResp.substring(axisLFIInfo.getBodyOffset());
+
+                                        // look for matches of our active check grep string
+                                        List<int[]> matchLFIAxis = getMatches(helpers.stringToBytes(lfiRespBody),
+                                                GREP_STRING_AXIS_XML, helpers);
+
+                                        if ((matchLFIAxis.size() > 0)) {
+                                            issues.add(new CustomScanIssue(
+                                                    baseRequestResponse.getHttpService(),
+                                                    axisURLLFI,
+                                                    new CustomHttpRequestResponse(axisLFITest, LFIResponse, baseRequestResponse.getHttpService()),
+                                                    TITLE_AXIS_LFI,
+                                                    DESCRIPTION_AXIS_LFI,
+                                                    "Update the Apache Axis component with the last stable release",
+                                                    Risk.High,
+                                                    Confidence.Certain
+                                            ));
+                                        }
+                                    }
                                 }
                             }
                         }
+
                     }
 
                 } catch (MalformedURLException ex) {
